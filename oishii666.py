96efa6b140a87e7c1668a4f0ec9982dbb93bc266
Johnny <milkkuan@gmail.com>
2022-05-02 20:37:33 +0800

    Fix: import error

--- oishii666.py
+++ oishii666.py
@@ -4,7 +4,11 @@ import json
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+    LocationMessage,
+    TemplateSendMessage, ButtonsTemplate, URITemplateAction,
+)
 
 import configparser
 
