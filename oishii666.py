e822cb418dc4acff102c0c14fc4691f245e064a9
Johnny <milkkuan@gmail.com>
2022-05-02 22:20:44 +0800

    Feat: Add carousel template message for receiving location message

--- oishii666.py
+++ oishii666.py
@@ -7,7 +7,12 @@ from linebot.exceptions import InvalidSignatureError
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
     LocationMessage,
-    TemplateSendMessage, ButtonsTemplate, URITemplateAction,
+    TemplateSendMessage,
+    ButtonsTemplate,
+    URITemplateAction,
+    CarouselTemplate,
+    CarouselColumn,
+    MessageAction
 )
 import configparser
 import random
@@ -85,14 +90,6 @@ def pretty_echo(event):
             TextSendMessage(text=pretty_text)
         )
 
-    else:
-        text=f'event.reply_token: {event.reply_token}\n' + f'event.source.user_id: {event.source.user_id}' + f'event.message.text: {event.message.text}'
-
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=text)
-        )
-
 @handler.add(MessageEvent, message=LocationMessage)
 def handle_location_message(event):
     # 獲取使用者的經緯度
@@ -101,8 +98,9 @@ def handle_location_message(event):
 
     # 使用 Google API Start =========
     # 1. 搜尋附近餐廳
-    nearby_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?key={}&location={},{}&rankby=distance&type=restaurant&language=zh-TW".format(GOOGLE_API_KEY, lat, long)
+    nearby_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?key={}&location={},{}&radius={}&type=restaurant&language=zh-TW".format(GOOGLE_API_KEY, lat, long, 1500)
     nearby_results = requests.get(nearby_url)
+    # next_page_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?pagetoken={}&key={}}".format(nearby_results.json()["next_page_token"], GOOGLE_API_KEY)
 
     # 隨機選擇一間餐廳
     nearby_restaurants_dict = nearby_results.json()
@@ -116,6 +114,7 @@ def handle_location_message(event):
         # 根據文件，最多只會有一張照片
         photo_reference = restaurant["photos"][0]["photo_reference"]
         thumbnail_image_url = "https://maps.googleapis.com/maps/api/place/photo?key={}&photoreference={}&maxwidth=1024".format(GOOGLE_API_KEY, photo_reference)
+    
     # 5. 組裝餐廳詳細資訊
     rating = "無" if restaurant.get("rating") is None else restaurant["rating"]
     address = "沒有資料" if restaurant.get("vicinity") is None else restaurant["vicinity"]
@@ -130,24 +129,78 @@ def handle_location_message(event):
     # 使用 Google API End =========
 
     # 回覆使用 Buttons Template
-    buttons_template_message = TemplateSendMessage(
-    alt_text=restaurant["name"],
-    template=ButtonsTemplate(
-            thumbnail_image_url=thumbnail_image_url,
-            title=restaurant["name"],
-            text=details,
-            actions=[
-                URITemplateAction(
-                    label='查看地圖',
-                    uri=map_url
+    # buttons_template_message = TemplateSendMessage(
+    # alt_text=restaurant["name"],
+    # template=ButtonsTemplate(
+    #         thumbnail_image_url=thumbnail_image_url,
+    #         title=restaurant["name"],
+    #         text=details,
+    #         actions=[
+    #             URITemplateAction(
+    #                 label='查看地圖',
+    #                 uri=map_url
+    #             ),
+    #         ]
+    #     )
+    # )
+
+    carousel_template_message = TemplateSendMessage(
+        alt_text=top20_restaurants[0]["name"],
+        template=CarouselTemplate(
+            columns=[
+                CarouselColumn(
+                    thumbnail_image_url=thumbnail_image_url,
+                    title=top20_restaurants[0]["name"],
+                    text="評分：{}\n地址：{}".format(top20_restaurants[0]["rating"], top20_restaurants[0]["vicinity"]),
+                    actions=[
+                        URITemplateAction(
+                            label='查看地圖',
+                            uri="https://www.google.com/maps/search/?api=1&query={lat},{long}&query_place_id={place_id}".format(
+                                lat=top20_restaurants[0]["geometry"]["location"]["lat"],
+                                long=top20_restaurants[0]["geometry"]["location"]["lng"],
+                                place_id=top20_restaurants[0]["place_id"]
+                            )
+                        ),
+                    ]
                 ),
+                CarouselColumn(
+                    thumbnail_image_url=thumbnail_image_url,
+                    title=top20_restaurants[1]["name"],
+                    text="評分：{}\n地址：{}".format(top20_restaurants[1]["rating"], top20_restaurants[1]["vicinity"]),
+                    actions=[
+                        URITemplateAction(
+                            label='查看地圖',
+                            uri="https://www.google.com/maps/search/?api=1&query={lat},{long}&query_place_id={place_id}".format(
+                                lat=top20_restaurants[1]["geometry"]["location"]["lat"],
+                                long=top20_restaurants[1]["geometry"]["location"]["lng"],
+                                place_id=top20_restaurants[1]["place_id"]
+                            )
+                        ),
+                    ]
+                ),
+                CarouselColumn(
+                    thumbnail_image_url=thumbnail_image_url,
+                    title=top20_restaurants[2]["name"],
+                    text="評分：{}\n地址：{}".format(top20_restaurants[2]["rating"], top20_restaurants[2]["vicinity"]),
+                    actions=[
+                        URITemplateAction(
+                            label='查看地圖',
+                            uri="https://www.google.com/maps/search/?api=1&query={lat},{long}&query_place_id={place_id}".format(
+                                lat=top20_restaurants[2]["geometry"]["location"]["lat"],
+                                long=top20_restaurants[2]["geometry"]["location"]["lng"],
+                                place_id=top20_restaurants[2]["place_id"]
+                            )
+                        ),
+                    ]
+                )
             ]
         )
     )
 
     line_bot_api.reply_message(
             event.reply_token,
-            buttons_template_message
+            # buttons_template_message
+            carousel_template_message
         )
 
 if __name__ == "__main__":
