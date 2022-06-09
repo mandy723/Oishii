f869963c52df9254f768391a6792c4130e9b8891
Zachary <tsai880806@gmail.com>
2022-06-09 22:50:41 +0800

    Change nearby search para type to food

--- oishii666.py
+++ oishii666.py
@@ -163,7 +163,7 @@ def handle_location_message(event):
     long = event.message.longitude
     radius = 1500
 
-    nearbyUrl = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?key={GOOGLE_API_KEY}&location={lat},{long}&radius={radius}&type=restaurant&language=zh-TW"
+    nearbyUrl = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?key={GOOGLE_API_KEY}&location={lat},{long}&radius={radius}&type=food&language=zh-TW"
 
     results = requests.get(nearbyUrl).json()
     nearbyResults = results["results"]
